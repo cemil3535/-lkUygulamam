1bffd2f136a9e31b04234e53c1a8e91e319a30ed
cemil3535 <cemilozcan35@gmail.com>
2024-07-17 15:07:55 +0300

    Add project files.

---
+++ İlkUygulamam/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Cemil Ozcan");
+Console.WriteLine("Universiteden mezun olup,egitimini aldigim fakat yapmak istemedigim(suanda da yapmadigim) meslekten; uzun zamandir aklimda olan 'Iste bu alanda calismak istiyorum' dedigim, ogrenmeyi istememe ve bunun icin ugrasmama(2. hatta 3. universiteye, sertifika, ve udemy kurslarina, youtube tutorial ragmen) 2013 yilindan beri basaramadim. Simdi Patika'ya guvenip bu istegimi gerceklestirecegimi dusundugum icin bootcamp katildim.");
